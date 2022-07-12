769863117c9999659a78c8edda23bd28eb5f6045
FD3S-Kyle <kdmm_14@hotmail.com>
2022-07-12 10:40:53 -0400

    Add the button and functionality to create a custom grid. Upon loading the page the default grid is a 16x16. Still have to style the page properly and make the square change to random rgb colors.

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+//creating custom grid size
+function getCustomGrid()
+{
+    let sizePrompt = prompt("What size grid would you like?");
+    let size = parseInt(sizePrompt);
+    let clearScreen = document.querySelector('.container').innerHTML = "";
+
+    if( size > 0)
+    {
+        const mainContainer = document.querySelector('.container');
+        mainContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+        mainContainer.style.gridTemplateRows = `repeat(${size}, auto)`;
+        const gridTile = document.createElement('div');
+        gridTile.classList.add("etchTile");
+        let clonedTile;
+
+        for(let col = 0; col < (size*size); col++)
+        {
+            clonedTile = gridTile.cloneNode();
+            mainContainer.appendChild(clonedTile);
+        }
+        addHoverEffect();
+    }
+    else
+    {
+        defaultGrid();
+        addHoverEffect();
+    }
+}
+
 //creating the 16x16 grid of divs
 function defaultGrid()
 {
     const mainContainer = document.querySelector('.container');
+    mainContainer.style.gridTemplateColumns = `repeat(16, auto)`;
+        mainContainer.style.gridTemplateRows = `repeat(16, auto)`;
     const gridTile = document.createElement('div');
     gridTile.classList.add("etchTile");
     let clonedTile;
@@ -13,5 +45,15 @@ function defaultGrid()
     }
 }
 
-defaultGrid();
+function addHoverEffect()
+{
+    const gridTiles = document.querySelectorAll('.etchTile');
+    gridTiles.forEach((eTile) => {
+        eTile.addEventListener('mouseenter', function(e){
+            e.target.style.background = 'blue'; 
+        });
+    });
+}
 
+defaultGrid();
+addHoverEffect();
